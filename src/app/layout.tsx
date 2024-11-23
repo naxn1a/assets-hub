1601fd087c1df4f47dca44ed0ccc9cce1eb177e1
Naxn1a <Persecond.sp@gmail.com>
2024-11-23 23:30:51 +0700

    ui component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,18 @@
 import type { Metadata } from "next";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Assets Hub",
   description: "A hub for all your assets",
 };
+
+const roboto = Roboto({
+  weight: ["400", "700"],
+  style: ["normal", "italic"],
+  subsets: ["latin"],
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -13,7 +21,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.className}>{children}</body>
     </html>
   );
 }
