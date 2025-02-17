d03eeba77468114fc08a30c1ec31c8d7e370fe79
naxn1a <persecond.sp@gmail.com>
2025-02-18 01:29:52 +0700

    remove select table

--- src/app/(routes)/asset/colums.tsx
+++ src/app/(routes)/asset/colums.tsx
@@ -7,28 +7,6 @@ import Link from "next/link";
 
 export const AssetColumns: ColumnDef<any>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
     accessorKey: "lot",
     header: ({ column }) => <TableColumnHeader column={column} title="Lot" />,
   },
--- src/app/(routes)/employee/colums.tsx
+++ src/app/(routes)/employee/colums.tsx
@@ -7,28 +7,6 @@ import Link from "next/link";
 
 export const EmployeeColumns: ColumnDef<any>[] = [
   {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={
-          table.getIsAllPageRowsSelected() ||
-          (table.getIsSomePageRowsSelected() && "indeterminate")
-        }
-        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
     accessorKey: "username",
     header: ({ column }) => (
       <TableColumnHeader column={column} title="Username" />
