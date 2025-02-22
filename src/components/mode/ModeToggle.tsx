6bbc0ff77755344be08826727674b6a8565062ee
naxn1a <persecond.sp@gmail.com>
2025-02-23 05:10:26 +0700

    fix theme mode

--- src/components/mode/ModeToggle.tsx
+++ src/components/mode/ModeToggle.tsx
@@ -1,17 +1,25 @@
 "use client";
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes";
 import { Moon, Sun } from "lucide-react";
 import { Button } from "@/components/ui/button";
 
 export function ModeToggle() {
   const { theme, setTheme } = useTheme();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) return null;
+
+  const onHandleClick = () => {
+    theme === "light" ? setTheme("dark") : setTheme("light");
+  };
 
   return (
-    <Button
-      variant="outline"
-      size="icon"
-      onClick={() => (theme === "light" ? setTheme("dark") : setTheme("light"))}
-    >
+    <Button variant="outline" size="icon" onClick={onHandleClick}>
       {theme === "light" ? <Sun /> : <Moon />}
     </Button>
   );
